ee54a2a376c9f64078e4a46c7cbadeb9373ba998
shiba841 <xsssx841@gmail.com>
2019-01-20 13:20:41 +0900

    ItemのID管理用スクリプト作成

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	[SerializeField] private string id;
+	[SerializeField] private new string name;
+
+	public string ID {
+		get { return id; }
+	}
+
+	public string Name {
+		get { return name; }
+	}
+
+}
