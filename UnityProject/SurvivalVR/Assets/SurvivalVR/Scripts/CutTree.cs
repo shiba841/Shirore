c065b1417721f654005e6ee0e8dab9e87fbfb0d2
shiba841 <xsssx841@gmail.com>
2019-01-25 23:58:41 +0900

    木の取得テスト

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/CutTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutTree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		Debug.Log(other.transform.position);
+	}
+}
