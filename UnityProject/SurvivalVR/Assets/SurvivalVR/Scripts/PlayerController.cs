e8bd77aadfb7de24b22025060587467625541b60
shiba841 <xsssx841@gmail.com>
2019-01-25 23:58:10 +0900

    プレイヤ操作テスト（未完）

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour {
+
+	public SteamVR_Input_Sources handType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if (SteamVR_Input._default.inActions.Teleport.)
+	}
+}
