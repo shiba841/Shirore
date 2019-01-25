56e0a9fbec5cf424e05d53a399717ddaa6ad59e0
shiba841 <xsssx841@gmail.com>
2019-01-25 23:58:31 +0900

    木の衝突判定取得テスト

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/TreeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour {
+
+	TerrainData terrainData;
+
+	// Use this for initialization
+	void Start () {
+		terrainData = this.GetComponent<Terrain>().terrainData;
+		var trees = terrainData.treeInstances;
+		foreach (var t in trees) {
+			Debug.Log(t.position);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		
+	}
+}
