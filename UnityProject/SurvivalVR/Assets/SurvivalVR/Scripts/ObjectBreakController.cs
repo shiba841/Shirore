5c270c3a209a70d6f6660b81c4e1ecbe1aacc64b
shiba841 <xsssx841@gmail.com>
2019-01-26 16:38:18 +0900

    木こり機能仮実装

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/ObjectBreakController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBreakController : MonoBehaviour {
+
+	public enum ObjectType {
+		none,
+		wood,
+		metal
+	}
+	[SerializeField] ObjectType objType = ObjectType.none;
+	public float maxDurability;
+	public GameObject particle;
+
+
+	// Use this for initialization
+	void Start () {
+		Durability = maxDurability;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter (Collision other) {
+		if (other.transform.root.tag.Equals("Tool")) {
+			Durability -= 10f;
+			Debug.Log(Durability);
+		}
+
+		if (Durability <= 0f) {
+			var rb = this.GetComponent<Rigidbody>();
+			rb.isKinematic = false;
+			rb.useGravity = true;
+		}
+	}
+
+	public float Durability { get; private set;	}
+}
