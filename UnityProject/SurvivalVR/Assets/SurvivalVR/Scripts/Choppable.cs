77f016b6e448d52b7bb75e5330bee1e663cd570b
shiba841 <xsssx841@gmail.com>
2019-01-28 19:01:50 +0900

    薪割り機能の追加

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/Choppable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Choppable : MonoBehaviour {
+
+	public GameObject choppedFirewood;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.transform.root.tag == "Tool") {
+			Chop();
+		}
+	}
+
+	private void Chop () {
+		var choppedObj = Instantiate(choppedFirewood, transform.position, transform.rotation);
+		choppedObj.transform.DetachChildren();
+		Destroy(choppedObj);
+		Destroy(this.gameObject);
+	}
+
+}
