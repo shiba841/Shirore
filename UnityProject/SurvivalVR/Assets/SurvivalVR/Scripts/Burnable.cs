178d162f7ee58b510a75f45936662d68718a804f
shiba841 <xsssx841@gmail.com>
2019-01-30 21:07:06 +0900

    焚き火機能の調整

---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/Burnable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Burnable : MonoBehaviour {
+
+	[SerializeField] private float burnTime = 0f;
+
+	private void Update () {
+		
+	}
+
+	public float BurnTime { get; }
+	
+}
---
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/BurnableSource.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableSource : MonoBehaviour {
+
+	public GameObject fire;
+	public float burnTime = 10;
+
+	private void Start () {
+		
+	}
+
+	private void Update () {
+		
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		var burnObj = other.gameObject;
+		var burnable = burnObj.GetComponent<Burnable>();
+
+		if (burnable != null) {
+			var burnRB = burnObj.GetComponent<Rigidbody>();
+			burnRB.constraints = RigidbodyConstraints.FreezeAll;
+		}
+	}
+
+	private void OnTriggerExit (Collider other) {
+		var burnObj = other.gameObject;
+		var burnable = burnObj.GetComponent<Burnable>();
+
+		if (burnable != null) {
+			var burnRB = burnObj.GetComponent<Rigidbody>();
+			burnRB.constraints = RigidbodyConstraints.None;
+		}
+	}
+}
