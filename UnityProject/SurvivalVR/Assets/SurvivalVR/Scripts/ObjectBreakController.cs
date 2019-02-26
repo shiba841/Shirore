5e2378dfa30672e2f57ba32468376cba204d5543
shiba841 <xsssx841@gmail.com>
2019-02-26 14:47:58 +0900

    木の耐久値表示機能を実装中

--- UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/ObjectBreakController.cs
+++ UnityProject/SurvivalVR/Assets/SurvivalVR/Scripts/ObjectBreakController.cs
@@ -12,8 +12,10 @@ public class ObjectBreakController : MonoBehaviour {
 	[SerializeField] ObjectType objType = ObjectType.none;
 	public float maxDurability;
 	public GameObject particleObj;
+	public GameObject durabilityPanel;
 
 	private ParticleSystem particle;
+	private TextMesh durabilityText;
 	private float destroyTime = 7f;
 	// Avoid consecutive hit
 	private float coolTime = 0.5f;
@@ -24,6 +26,7 @@ public class ObjectBreakController : MonoBehaviour {
 	void Start () {
 		Durability = maxDurability;
 		particle = particleObj.GetComponent<ParticleSystem>();
+		durabilityText = durabilityPanel.GetComponent<TextMesh>();
 	}
 	
 	private float hitTime;
@@ -32,6 +35,8 @@ public class ObjectBreakController : MonoBehaviour {
 		if (Time.time - hitTime > coolTime) {
 			hittable = true;
 		}
+
+		DisplayDurability(displayDurability);
 	}
 
 	private void OnCollisionEnter (Collision other) {
@@ -43,13 +48,28 @@ public class ObjectBreakController : MonoBehaviour {
 			}
 			Durability -= 10f;
 
-			// Debug.Log(Durability);
-		}
-
-		if (Durability <= 0f) {
-			if (objType == ObjectType.wood) {
-				TreeFelling();
+			if (Durability <= 0f) {
+				if (objType == ObjectType.wood) {
+					TreeFelling();
+				}
 			}
+		}
+	}
+
+	private void OnCollisionStay(Collision other) {
+		if (other.transform.name.Equals("GUICensor")) {
+			Debug.Log("disp");
+			displayDurability = true;
+			foreach (var point in other.contacts) {
+				displayPos = point.point;
+			}
+		}
+		
+	}
+
+	private void OnCollisionExit(Collision other) {
+		if (other.transform.name.Equals("GUICensor")) {
+			displayDurability = false;
 		}
 	}
 
@@ -58,6 +78,21 @@ public class ObjectBreakController : MonoBehaviour {
 		rb.isKinematic = false;
 		rb.useGravity = true;
 		Destroy(this.gameObject, destroyTime);
+	}
+
+	private Vector3 displayPos;
+	private bool displayDurability = false;
+
+	private void DisplayDurability (bool disp) {
+		if (disp) {
+			durabilityPanel.transform.position = displayPos;
+			durabilityPanel.transform.LookAt(Camera.main.transform);
+			durabilityText.text = string.Format("{0} / {1}", Durability, maxDurability);
+			durabilityPanel.SetActive(true);
+		}
+		else {
+			durabilityPanel.SetActive(false);
+		}
 	}
 
 	private void PlayParticle (Vector3 hitPos) {
